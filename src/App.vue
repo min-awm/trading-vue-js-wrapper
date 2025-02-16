a8e874f71ba3c30383161c85ed46244d4ee37580
min-awm <minha05112003a@gmail.com>
2025-02-16 14:38:11 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <chart></chart>
+    {{ ab }}
+  </div>
+</template>
+
+<script>
+const App = {
+  components: {
+    chart: () => import("@/components/b.vue"),
+  },
+  data() {
+    return {
+      ab: "3323232",
+    };
+  },
+  methods: {
+    globalFunction() {
+      this.ab = "ddsds";
+      console.log("Function called from main.js!");
+    },
+  },
+};
+export default App;
+</script>
---
+++ src/main.js
@@ -0,0 +1,54 @@
+import Vue from "vue";
+import App from "./components/b.vue";
+import store from "@/store.json";
+
+export default class TradeChart {
+  constructor(element) {
+    this.app = new Vue({
+      render: (h) => h(App),
+    }).$mount(element);
+  }
+
+  setSize(height, width) {
+    console.log(this.app)
+    store.config.height = height;
+    store.config.width = width;
+  }
+
+  setName(name) {
+    store.config.name = name;
+  }
+
+  setChartData(value) {
+    store.chartData = value;
+  }
+
+  showEMA5(value) {
+    store.indicators.ema5 = value;
+  }
+
+  showEMA10(value) {
+    store.indicators.ema10 = value;
+  }
+}
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+const a = new TradeChart(root);
+a.setSize(500, 800);
+// console.log(store);
+
+// let i = 0;
+// const ab = [];
+// setInterval(() => {
+//   ab.push([
+//     1621846900000, 130, 127.0073013305664, 125.94000244140625,
+//     126.65499877929688, 11289029,
+//   ]);
+//   a.setChartData(ab);
+//   i += 1;
+// }, 10);
+
+// setTimeout(() => {
+//   a.showEMA10(true)
+// }, 1000);
